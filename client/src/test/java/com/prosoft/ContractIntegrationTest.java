e13395adff7dcf64cb2c064574bf8c3e8f79b7bb
sproshchaev <sproshchaev@gmail.com>
2024-11-04 20:39:03 +0500

    add logger

--- client/src/test/java/com/prosoft/ContractIntegrationTest.java
+++ client/src/test/java/com/prosoft/ContractIntegrationTest.java
@@ -1,6 +1,8 @@
 package com.prosoft;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
 import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
@@ -10,10 +12,12 @@ import org.springframework.web.client.RestTemplate;
 @AutoConfigureStubRunner(ids = {"com.prosoft:server:+:stubs:8100"}, stubsMode = StubRunnerProperties.StubsMode.LOCAL)
 class ContractIntegrationTest {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ContractIntegrationTest.class);
+
     @Test
     void getString() {
         RestTemplate restTemplate = new RestTemplate();
-        System.out.println(restTemplate.getForObject("http://localhost:8100/hello", String.class));
+        LOGGER.info(restTemplate.getForObject("http://localhost:8100/hello", String.class));
     }
 
 }
