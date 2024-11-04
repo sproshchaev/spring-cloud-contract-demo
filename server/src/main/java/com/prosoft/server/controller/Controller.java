d666e832f4db62b4831e4a42c179d128f1497ebf
sproshchaev <sproshchaev@gmail.com>
2024-11-04 20:49:59 +0500

    fix

--- server/src/main/java/com/prosoft/server/controller/Controller.java
+++ server/src/main/java/com/prosoft/server/controller/Controller.java
@@ -1,14 +1,19 @@
 package com.prosoft.server.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Controller.class);
+
+
     @GetMapping("/hello")
     public String hello() {
-        System.out.println("request");
+        LOGGER.info("incoming request");
         return "Hello!";
     }
 
