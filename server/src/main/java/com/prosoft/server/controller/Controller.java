f1ea3d879f7e710eaf5b633e488dad43368822e1
sproshchaev <sproshchaev@gmail.com>
2024-11-08 15:48:27 +0500

    add post /hello

--- server/src/main/java/com/prosoft/server/controller/Controller.java
+++ server/src/main/java/com/prosoft/server/controller/Controller.java
@@ -3,6 +3,8 @@ package com.prosoft.server.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,8 +14,14 @@ public class Controller {
 
     @GetMapping("/hello")
     public String hello() {
-        LOGGER.info("incoming request");
+        LOGGER.info("Incoming GET request to /hello");
         return "Hello!";
+    }
+
+    @PostMapping("/hello")
+    public String helloWithName(@RequestBody String name) {
+        LOGGER.info("Incoming POST request to /hello with name: {}", name);
+        return String.format("Hello, %s!", name);
     }
 
 }
