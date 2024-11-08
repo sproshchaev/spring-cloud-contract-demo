ae281f0b1326f462b2c2d58f24d8cd1ca8071045
sproshchaev <sproshchaev@gmail.com>
2024-11-08 16:38:02 +0500

    add comment

--- server/src/test/resources/contracts/groovy-example/get_hello_contract.groovy
+++ server/src/test/resources/contracts/groovy-example/get_hello_contract.groovy
@@ -2,6 +2,13 @@ package contracts
 
 import org.springframework.cloud.contract.spec.Contract
 
+/**
+ * Этот контракт тестирует базовый ответ от сервиса
+ * на GET-запрос к эндпоинту `/hello`.
+ * Ожидается, что сервис вернет строку "Hello!" в теле ответа
+ * при GET-запросе на `/hello`.
+ * Для его использования необходимо переместить в resources\contracts
+ */
 Contract.make {
     description "should return hello for GET request"
 
--- server/src/test/resources/contracts/groovy-example/post_hello_contract.groovy
+++ server/src/test/resources/contracts/groovy-example/post_hello_contract.groovy
@@ -2,6 +2,13 @@ package contracts
 
 import org.springframework.cloud.contract.spec.Contract
 
+/**
+ * Этот контракт тестирует персонализированный ответ от сервиса
+ * на POST-запрос к эндпоинту `/hello`, когда в теле запроса передано имя.
+ * Ожидается, что сервис вернет строку "Hello, John Doe!" в теле ответа
+ * при POST-запросе на `/hello` с телом "John Doe".
+ * Для его использования необходимо переместить в resources\contracts
+ */
 Contract.make {
     description "should return personalized hello for POST request"
 
